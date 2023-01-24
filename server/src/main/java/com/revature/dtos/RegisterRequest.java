1b70d825a08f5c83a59946fbfce8c766139c8e10
Andy Tran <105564473+AndyITZ1@users.noreply.github.com>
2023-01-24 16:52:42 -0500

    Backend setup done!

--- server/src/main/java/com/revature/dtos/RegisterRequest.java
+++ server/src/main/java/com/revature/dtos/RegisterRequest.java
@@ -1,15 +1,71 @@
 package com.revature.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class RegisterRequest {
     private String email;
     private String password;
     private String firstName;
     private String lastName;
+    private String username;
+
+    public RegisterRequest() {
+    }
+
+    public RegisterRequest(String email, String password, String firstName, String lastName, String username) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterRequest{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
--- server/src/main/java/com/revature/models/Post.java
+++ server/src/main/java/com/revature/models/Post.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "posts")
 public class Post {
 
@@ -26,4 +19,96 @@ public class Post {
 	private User author;
 
 	private PostType postType;
+	private int likes;
+
+	public Post() {
+	}
+
+	public Post(int id, String text, String imageUrl, List<Post> comments, User author, PostType postType, int likes) {
+		this.id = id;
+		this.text = text;
+		this.imageUrl = imageUrl;
+		this.comments = comments;
+		this.author = author;
+		this.postType = postType;
+		this.likes = likes;
+	}
+
+	public Post(String text, String imageUrl, List<Post> comments, User author, PostType postType) {
+		this.text = text;
+		this.imageUrl = imageUrl;
+		this.comments = comments;
+		this.author = author;
+		this.postType = postType;
+		this.likes = 0;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public List<Post> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Post> comments) {
+		this.comments = comments;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public PostType getPostType() {
+		return postType;
+	}
+
+	public void setPostType(PostType postType) {
+		this.postType = postType;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+
+	@Override
+	public String toString() {
+		return "Post{" +
+				"id=" + id +
+				", text='" + text + '\'' +
+				", imageUrl='" + imageUrl + '\'' +
+				", comments=" + comments +
+				", author=" + author +
+				", postType=" + postType +
+				", likes=" + likes +
+				'}';
+	}
 }
