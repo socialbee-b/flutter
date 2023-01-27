f4af8b6a5ff2fd531384aa868eb69807be00277f
Andy Tran <105564473+AndyITZ1@users.noreply.github.com>
2023-01-27 11:45:35 -0500

    Backend Postman Testing Setup

--- server/src/main/java/com/revature/controllers/UserController.java
+++ server/src/main/java/com/revature/controllers/UserController.java
@@ -4,13 +4,13 @@ import com.revature.models.User;
 import com.revature.services.PostService;
 import com.revature.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/users")
 @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"}, allowCredentials = "true")
 public class UserController {
 
@@ -20,7 +20,24 @@ public class UserController {
         this.userService = userService;
     }
 
-//    public ResponseEntity<User> upsertUser (@RequestBody User user) {
-//
-//    } 
+    // Testing Method: Adds a User
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(this.userService.save(user));
+    }
+
+    // Testing Method: Get All Users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(this.userService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        Optional<User> userOptional = userService.findById(id);
+        if (!userOptional.isPresent()) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(userOptional.get());
+    }
 }
