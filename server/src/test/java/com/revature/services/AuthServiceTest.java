e9d9aa6d34dd9cb0fc385ec8b5efd95d38849bc2
DavidMMata <davidmmata89@gmail.com>
2023-02-02 10:48:14 -0700

    testing for authservice

--- server/src/test/java/com/revature/services/AuthServiceTest.java
+++ server/src/test/java/com/revature/services/AuthServiceTest.java
@@ -1,27 +1,75 @@
 package com.revature.services;
 
-import com.revature.repositories.PostRepository;
-import com.revature.repositories.UserRepository;
+import com.revature.models.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
 
     @Mock
-    private UserRepository userRepository;
-    @Mock
-    private PostService postService;
+    private UserService userService;
+
     @Test
-    void findByCredentials() {
-        UserService userService = new UserService(userRepository,postService);
+    void findByCredentialsSuccess() {
+        AuthService authService = new AuthService(userService);
+        User mockUser = new User ("blorp@email.com","blorp","david","mata", "norping");
+
+        when(authService.findByCredentials("blorp@email.com", "blorp")).thenReturn(Optional.of(mockUser));
+
+        Optional<User> resultUser = authService.findByCredentials("blorp@email.com", "blorp");
+
+        assertEquals(resultUser.get().getEmail(),mockUser.getEmail());
+        assertEquals(resultUser.get().getPassword(),mockUser.getPassword());
     }
 
     @Test
-    void register() {
+    void findByCredentialsFail() {
+        AuthService authService = new AuthService(userService);
+        User mockUser = new User ("blorp@email.com","blorp","david","mata", "norping");
+
+        when(authService.findByCredentials("blorp@email.com", "blorp")).thenReturn(Optional.of(mockUser));
+
+        Optional<User> resultUser = authService.findByCredentials("blorp@email.com", "blorp");
+
+        assertNotEquals("blorpnorp",mockUser.getEmail());
+        assertNotEquals("chicaca",mockUser.getPassword());
+
+        assertNotEquals(resultUser.get().getEmail(),"blorpnorp");
+        assertNotEquals(resultUser.get().getPassword(),"chicaca");
+
+    }
+
+    @Test
+    void registerSuccess() {
+        AuthService authService = new AuthService(userService);
+        User mockUser = new User ("blorp@email.com","blorp","david","mata", "norping");
+
+        when(authService.register(mockUser)).thenReturn(mockUser);
+
+        User rUser = authService.register(mockUser);
+
+        verify(userService).save(mockUser);
+        assertEquals(mockUser,rUser);
+    }
+
+    @Test
+    void registerFail() {
+        AuthService authService = new AuthService(userService);
+        User mockUser = new User ("blorp@email.com","blorp","david","mata", "norping");
+
+        when(authService.register(mockUser)).thenThrow(new IllegalArgumentException());
+
+        assertThrows(IllegalArgumentException.class, () -> authService.register(mockUser));
+        verify(userService).save(mockUser);
     }
 }
