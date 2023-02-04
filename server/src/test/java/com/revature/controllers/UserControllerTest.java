fb7924b64b802dac8ff9fe09c286d8092ee937de
RebelWright <rebeltrentwrightjr@gmail.com>
2023-02-04 16:05:08 -0600

    Adding more tests

--- server/src/test/java/com/revature/controllers/UserControllerTest.java
+++ server/src/test/java/com/revature/controllers/UserControllerTest.java
@@ -1,10 +1,10 @@
 package com.revature.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.Post;
+import com.revature.models.PostType;
 import com.revature.models.User;
 import com.revature.services.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,11 +17,12 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItems;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
@@ -35,12 +36,6 @@ public class UserControllerTest {
     //response from the servlet
     @Autowired
     private ObjectMapper objectMapper;
-    @BeforeEach
-    void setup(){
-        User testUser1 = new User("test.com", "password", "John", "Doe", "JDoe");
-
-    }
-
 
     @Test
     public void createUser_Successful() throws Exception {
@@ -55,12 +50,12 @@ public class UserControllerTest {
         User testUser2 = new User(2,"test2.com","password2","Bob","Smith","BSmi",null,null,"image2.com");
         //Using this mockMvc perform a post request to the "/users" URI endpoint in controller
         this.mockMvc.perform(post("/users")
-                //we want our request to be Application_JSON mediatype for our server to read it
-                //used because we are performing a post request
-                .contentType(MediaType.APPLICATION_JSON)
-                 //We use the object mapper we called earlier and it's method writeValueAsString to actually
-                //convert our user object to a JSon string
-                .content(objectMapper.writeValueAsString(testUser2)))
+                        //we want our request to be Application_JSON mediatype for our server to read it
+                        //used because we are performing a post request
+                        .contentType(MediaType.APPLICATION_JSON)
+                        //We use the object mapper we called earlier and it's method writeValueAsString to actually
+                        //convert our user object to a JSon string
+                        .content(objectMapper.writeValueAsString(testUser2)))
                 //we are now defining what HTTP response is expected to return from our Http request to the servlet
                 //in this case a status of 200 when request is ok
                 .andExpect(status().is(200))
@@ -76,6 +71,15 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.lastName", is(testUser2.getLastName())))
                 .andExpect(jsonPath("$.username", is(testUser2.getUsername())))
                 .andExpect(jsonPath("$.imageUrl", is(testUser2.getImageUrl())));
+    }
+    @Test
+    void createNewUserWithoutEmailFail() throws Exception {
+        User testUser2 = new User(2, null, "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
+        String requestBody = objectMapper.writeValueAsString(testUser2);
+        this.mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isBadRequest());
     }
 
 
@@ -108,6 +112,7 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.imageUrl", is(testUser2.getImageUrl())));
     }
 
+
     @Test
     void findByUsername() throws Exception {
         User testUser2 = new User(2, "test2.com", "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
@@ -126,47 +131,147 @@ public class UserControllerTest {
 
     @Test
     void editPassword() throws Exception {
+        //creating test user
         User testUser2 = new User(2, "test2.com", "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
-        String editString = "password3";
-        String requestBody = objectMapper.writeValueAsString(editString);
+        //creating edited Password string
+        String editPassword = "password3";
+        //Using object mapper to write editPassword as a JSON string
+        String requestBody = objectMapper.writeValueAsString(editPassword);
+        //running the userService find by Id (2 in our test user) and returning Optional of test user
         given(userService.findById(2)).willReturn(Optional.of(testUser2));
+        //setting the test user's password as the new edited password
+        testUser2.setPassword(editPassword);
+        //when running save method in user service, will return our test user
         given(userService.save(testUser2)).willReturn((testUser2));
-        this.mockMvc.perform(put("/users/2/password"))
+        this.mockMvc.perform(put("/users/2/password")
+                        //we're sending JSon data
+                        .contentType(MediaType.APPLICATION_JSON)
+                        //our content we are sending is our JSON string of the edited password
+                        .content(requestBody))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(2))
-                .andExpect(jsonPath("$.password").value("password3"));
+                //we are expecting Json data as a response
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                //we are expecting our response to contain an id of 2 and our new edited password
+                .andExpect(jsonPath("$.id", is(testUser2.getId())))
+                .andExpect(jsonPath("$.password", is(testUser2.getPassword())));
+
     }
 
     @Test
-    void editEmail() {
+    void editEmail() throws Exception {
         User testUser2 = new User(2, "test2.com", "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
         String editEmail = "com.test2";
         String requestBody = objectMapper.writeValueAsString(editEmail);
         given(userService.findById(2)).willReturn(Optional.of(testUser2));
+        testUser2.setEmail(editEmail);
         given(userService.save(testUser2)).willReturn((testUser2));
-        this.mockMvc.perform(put("/users/2/password"))
+        this.mockMvc.perform(put("/users/2/email")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(2))
-                .andExpect(jsonPath("$.password").value("password3"));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testUser2.getId())))
+                .andExpect(jsonPath("$.email", is(testUser2.getEmail())));
     }
 
     @Test
-    void editUsername() {
+    void editUsername() throws Exception {
+        String editUsername = "IMsb";
+        User testUser2 = new User(2, "test2.com", "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
+        String requestBody = objectMapper.writeValueAsString(editUsername);
+        given(userService.findById(2)).willReturn(Optional.of(testUser2));
+        testUser2.setUsername(editUsername);
+        given(userService.save(testUser2)).willReturn((testUser2));
+        this.mockMvc.perform(put("/users/2/username")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testUser2.getId())))
+                .andExpect(jsonPath("$.username", is(testUser2.getUsername())));
     }
 
     @Test
-    void updateImageUrl() {
+    void updateImageUrl() throws Exception {
+        User testUser2 = new User(2, "test2.com", "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
+        String editImageUrl = "com.image";
+        String requestBody = objectMapper.writeValueAsString(editImageUrl);
+        given(userService.findById(2)).willReturn(Optional.of(testUser2));
+        testUser2.setImageUrl(editImageUrl);
+        given(userService.save(testUser2)).willReturn((testUser2));
+        this.mockMvc.perform(put("/users/2/profileImage")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testUser2.getId())))
+                .andExpect(jsonPath("$.imageUrl", is(testUser2.getImageUrl())));
+    }
+    //still needs work....
+    @Test
+    void addFollower() throws Exception{
+        User testUser1 = new User("test.com", "password", "John", "Doe", "JDoe");
+        testUser1.setId(1);
+        testUser1.setFollowers(new ArrayList<>());
+        testUser1.setFollowing(new ArrayList<>());
+
+        User testUser2 = new User(2, "test2.com", "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
+        testUser2.setFollowers(new ArrayList<>());
+        testUser2.setFollowing(new ArrayList<>());
+        List<User> followSuccess = new ArrayList<>();
+        followSuccess.add(testUser1);
+        followSuccess.add(testUser2);
+        given(userService.addFollower(testUser1, testUser2)).willReturn(followSuccess);
+
+        given(userService.findById(1)).willReturn(Optional.of(testUser1));
+        given(userService.findById(2)).willReturn(Optional.of(testUser2));
+        mockMvc.perform(put("/users/1/follow")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("2"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[*].id", hasItems(testUser1.getId(), testUser2.getId())))
+                .andExpect(jsonPath("$.length()", is(2)));
     }
 
     @Test
-    void addFollower() {
+    void removeFollower() throws Exception {
+        User followedUser = new User("test.com", "password", "John", "Doe", "JDoe");
+        User follower = new User(2, "test2.com", "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
+
+        List<User> removeSuccess = new ArrayList<>();
+        removeSuccess.add(followedUser);
+
+        given(userService.findById(1)).willReturn(Optional.of(followedUser));
+        given(userService.findById(2)).willReturn(Optional.of(follower));
+        given(userService.removeFollower(followedUser, follower)).willReturn(removeSuccess);
+
+        mockMvc.perform(put("/users/1/unfollow")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("2"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id", is(followedUser.getId())));
+
     }
 
     @Test
-    void removeFollower() {
+    void getFeedForUser() throws Exception {
+        User testUser1 = new User("test.com", "password", "John", "Doe", "JDoe");
+        User testUser2 = new User(2, "test2.com", "password2", "Bob", "Smith", "BSmi", null, null, "image2.com");
+        testUser1.setId(1);
+        Optional<User> optionalUser = Optional.of(testUser1);
+        when(userService.findById(1)).thenReturn(optionalUser);
+        List<Post> feed = new ArrayList<>();
+        feed.add(new Post(1,"This is a test post","image.com",new ArrayList<>(), testUser2, PostType.Top, 1));
+        feed.add(new Post(2,"Test post 2","image2.com",new ArrayList<>(), testUser2, PostType.Top, 1));
+        when(userService.getFeedForUser(optionalUser.get())).thenReturn(feed);
+
+        mockMvc.perform(get("/users/1/feed"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(feed.size())))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[1].id", is(2)));
     }
 
-    @Test
-    void getFeedForUser() {
-    }
 }
